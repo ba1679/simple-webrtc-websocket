84a20ce41209fffc3ae98051f0e1e5abc670d9ff
Render Lai <render.lai@alfred.camera>
2023-02-16 10:57:35 +0800

    chore: backend setup

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const app = require("express")();
-const server = require("http").createServer(app);
-const cors = require("cors");
 
-const io = require("socket.io")(server, {
-	cors: {
-		origin: "*",
-		methods: [ "GET", "POST" ]
-	}
+const app = require('express')();
+const server = require('http').createServer(app);
+const cors = require('cors');
+
+
+const io =  require('socket.io')(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
 });
 
 app.use(cors());
@@ -14,23 +16,40 @@ app.use(cors());
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-	res.send('Running');
+  res.send('Running');
 });
 
-io.on("connection", (socket) => {
-	socket.emit("me", socket.id);
+function findNowRoom (client) {
+  const clientRooms = Array.from(client.rooms);
+  return clientRooms.find((item) => {
+    return item !== client.id;
+  });
+}
 
-	socket.on("disconnect", () => {
-		socket.broadcast.emit("callEnded")
-	});
+io.on('connection', (client) => {
+  console.log(`socket 用戶連接 ${client.id}`);
 
-	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
-	});
+  client.on('joinRoom', (room) => {
+    console.log(room);
 
-	socket.on("answerCall", (data) => {
-		io.to(data.to).emit("callAccepted", data.signal)
-	});
+    const nowRoom = findNowRoom(client);
+    if (nowRoom) {
+      client.leave(nowRoom);
+    }
+    client.join(room);
+    client.to(room).emit('roomBroadcast', '已有新人加入聊天室！')
+  });
+
+  client.on('peerConnectSignaling', (message) => {
+    console.log('接收資料：', message);
+
+    const nowRoom = findNowRoom(client);
+    client.to(nowRoom).emit('peerConnectSignaling', message);
+  });
+
+  client.on('disconnect', () => {
+    console.log(`socket 用戶離開 ${client.id}`);
+  });
 });
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
